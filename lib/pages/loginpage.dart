e36518d33d67d55a4e1039a77d69493ab0120beb
dev chauhan <chauhan97.dev@gmail.com>
2021-08-14 15:13:21 +0530

    Day 5 completed

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 class Loginpage extends StatelessWidget {
   const Loginpage({Key? key}) : super(key: key);
@@ -8,35 +9,46 @@ class Loginpage extends StatelessWidget {
     return Material(
         color: Colors.white,
         child: Center(
-            child: Column(
-          children: [
-            Image.asset(
-              "images/login_image.PNG",
-              fit: BoxFit.cover,
-            ),
-            Text(
-              "Welcome",
-              style: TextStyle(),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                        hintText: "Enter UserName", labelText: "UserName"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Enter password", labelText: "Password"),
-                  )
-                ],
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                "images/login_image.PNG",
+                fit: BoxFit.cover,
               ),
-            ),
-            ElevatedButton(onPressed: () {}, child: Text("Login"))
-          ],
+              Text(
+                "Welcome",
+                style: TextStyle(fontSize: 28),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                          hintText: "Enter UserName", labelText: "UserName"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          hintText: "Enter password", labelText: "Password"),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print("hello therer");
+                    Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  },
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(minimumSize: Size(150, 40))),
+            ],
+          ),
         )));
   }
 }
